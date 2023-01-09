342e62194cb7a9580c22e5eabb019f77332f09be
dhiwakar1172 <107527878+dhiwakar1172@users.noreply.github.com>
2023-01-09 23:48:00 +0530

    Update ELBClientBuilder.java

--- main/java/org/ClientBuilder/ELBClientBuilder.java
+++ main/java/org/ClientBuilder/ELBClientBuilder.java
@@ -8,7 +8,7 @@ import com.amazonaws.services.elasticloadbalancingv2.AmazonElasticLoadBalancingC
 public class ELBClientBuilder {
 
     public static AmazonElasticLoadBalancing initialize(){
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIASHV2H2D77UVEDTFF", "PmvEnEk+Lb8Si/JfeZ4Bd5nVt4MJ2QEdIV20KKfB");
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("//", "//");
         AmazonElasticLoadBalancing elb = AmazonElasticLoadBalancingClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
         return elb;
     }
